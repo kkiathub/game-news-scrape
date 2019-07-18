002e1e6c456af2983aa0b461322165e03a384d64
J Frost <icefang@hotmail.com>
2019-07-18 01:37:22 -0700

    scrape data , findall, deletemany

---
+++ controllers/mongoController.js
@@ -0,0 +1,114 @@
+const express = require("express");
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const router = express.Router();
+
+// Import the models to use its database functions.
+const db = require("../models");
+
+// A GET route for scraping the echoJS website
+router.get("/scrape", (req, res) => {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.gamespot.com/").then((response) => {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    const $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article.media-article").each(function(i, element) {
+      // Save an empty result object
+      const result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .find(".media-title")
+        .text();
+      result.summary = $(this)
+        .find(".media-deck")
+        .text();
+      result.link = "https://www.gamespot.com" + $(this)
+        .children("a")
+        .attr("href");
+      result.image = $(this)
+        .find("img")
+        .attr("src");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then((dbArticle) => {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch((err) => {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete");
+  });
+});
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", (req, res) => {
+  db.Article.find({})
+  .then((dbArticle) => {
+    // If all Users are successfully found, send them back to the client
+    const hbsObject = {
+      articles: dbArticle
+    };
+    res.render("index", hbsObject);
+
+  })
+  .catch((err) => {
+    // If an error occurs, send the error back to the client
+    res.json(err);
+  });
+});
+
+// delete all documents in Article.
+router.delete("/api/clear", (req, res) => {
+
+  db.Article.deleteMany({},
+    (err, result) => {
+      res.status(200).end();
+    }
+  );
+});
+/*
+router.post("/api/cats", (req, res) => {
+  cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], result => {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/cats/:id", (req, res) => {
+  const condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cat.update(
+    {
+      sleepy: req.body.sleepy
+    },
+    condition,
+    result => {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
+});
+
+
+*/
+// Export routes for server.js to use.
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// Require all models
+const db = require("./models");
+
+const PORT = 3000;
+
+// Initialize Express
+const app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/mongoController.js");
+app.use(routes);
+
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/hw14mongo";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Routes
+
+
+
+// Route for getting all Articles from the db
+app.get("/articles", (req, res) => {
+  // TODO: Finish the route so it grabs all of the articles
+  db.Article.find({})
+  .then((dbArticle) => {
+    // If all Users are successfully found, send them back to the client
+    res.json(dbArticle);
+  })
+  .catch((err) => {
+    // If an error occurs, send the error back to the client
+    res.json(err);
+  });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", (req, res) => {
+  // TODO
+  // ====
+  // Finish the route so it finds one article using the req.params.id,
+  // and run the populate method with "note",
+  // then responds with the article with the note included
+  db.Article.findOne(  { _id: req.params.id })
+  .populate("note")
+  .then((dbArticle) => {
+    // If all Notes are successfully found, send them back to the client
+    console.log(dbArticle);
+    res.json(dbArticle);
+  })
+  .catch((err) => {
+    // If an error occurs, send the error back to the client
+    res.json(err);
+  });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", (req, res) => {
+  // TODO
+
+  db.Note.create(req.body)
+  .then((dbNote) => {
+    // then find an article from the req.params.id
+    // and update it's "note" property with the _id of the new note
+
+    return db.Article.findOneAndUpdate( { _id: req.params.id }, { $set: { note: dbNote._id } }, { new: true });
+  })
+  .then((dbArticle) => {
+    // If the User was updated successfully, send it back to the client
+    res.json(dbArticle);
+  })
+  .catch((err) => {
+    // If an error occurs, send it back to the client
+    res.status(500).json(err);
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log("App running on port " + PORT + "!");
+});
